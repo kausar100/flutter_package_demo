4ab534de9b32f9c8be19b5f782b04f951f7b7383
Md. Golam Kausar <golam@mirinfosys.com>
2024-01-22 17:55:46 +0600

    custom scrollview with sliver list/ slivergrid delegate

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const FloatingAppBar());
+  }
+}
+
+class FloatingAppBar extends StatefulWidget {
+  const FloatingAppBar({super.key});
+
+  @override
+  State<FloatingAppBar> createState() => _FloatingAppBarState();
+}
+
+class _FloatingAppBarState extends State<FloatingAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // No appBar property provided, only the body.
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 8.0,
+        child: CustomScrollView(
+          slivers: [
+            // Add the app bar to the CustomScrollView.
+            SliverAppBar(
+              // Provide a standard title.
+              title: const Text("Do you like it!"),
+              // Allows the user to reveal the app bar if they begin scrolling
+              // back up the list of items.
+              floating: true,
+              // Display a placeholder widget to visualize the shrinking size.
+              flexibleSpace: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Image.asset(
+                  "images/bg_photo.jpg",
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              // Make the initial height of the SliverAppBar larger than normal.
+              expandedHeight: 200,
+            ),
+            // Next, create a SliverList
+            SliverGrid(
+              // Use a delegate to build items as they're scrolled on screen.
+              delegate: SliverChildBuilderDelegate(
+                // The builder function returns a ListTile with a title that
+                // displays the index of the current item.
+                (context, index) => Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: ListTile(title: Text('Item #$index'))),
+                // Builds 1000 ListTiles
+                childCount: 10,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+            ),
+            // Next, create a SliverList
+            SliverList(
+              // Use a delegate to build items as they're scrolled on screen.
+              delegate: SliverChildBuilderDelegate(
+                // The builder function returns a ListTile with a title that
+                // displays the index of the current item.
+                (context, index) => Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: ListTile(title: Text('Item #$index'))),
+                // Builds 1000 ListTiles
+                childCount: 20,
+              ),
+            ),
+          ],
+        ),
       ),
-      home: const IsolateParsingApp(title: "Background Parsing")
-    );
+    ));
   }
 }
